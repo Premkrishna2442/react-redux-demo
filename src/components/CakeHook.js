457db411a92663e08fee2637799ebff63656fe20
premkrishna2442 <premkrishna222@gmail.com>
2022-05-24 14:24:16 +0530

    first

---
+++ src/components/CakeHook.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake, restockCake } from "./redux";
+function CakeHook() {
+  const noofcakes = useSelector((state) => state.noofCakes);
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  return (
+    <div>
+      {noofcakes < 1 ? <h2>Out OF Stock</h2> : <h2>No of Cakes {noofcakes}</h2>}
+      <button onClick={() => dispatch(buyCake(count))}>buy cake </button>
+      <input
+        type="String"   
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+      />
+      <button onClick={() => dispatch(restockCake(count))}>
+        restock cake{" "}
+      </button>
+    </div>
+  );
+}
+
+export default CakeHook;
